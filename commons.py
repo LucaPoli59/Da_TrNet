6bd29d1369ce462108f9d21b8714f82e05739642
Luca Poli <poli.luca.marconi@gmail.com>
2024-02-10 16:13:28 +0100

    Nodes edges load bug fixed, Add function for attacks summary plotting to commons.py

--- commons.py
+++ commons.py
@@ -21,6 +21,7 @@ APP_PATH = os.path.join(CWD, "app")
 RESULTS_PATH = os.path.join(APP_PATH, 'data')
 ATTACKS_PATH = os.path.join(RESULTS_PATH, 'attacks')
 
+
 ########################################################################################
 ################### General Functions ##################################################
 ########################################################################################
@@ -30,6 +31,27 @@ def load_graph(src=GTFS_PATH_OTHER, feed_id=1139, feed_name="DB", start_time=7, 
     feed = ptr.get_representative_feed(feed_path)
     graph = ptr.load_feed_as_graph(feed, start_time=start_time * 3600, end_time=end_time * 3600, name=feed_name)
     return graph
+
+
+def graph_to_gdfs(g):
+    """Converts a graph to nodes and edges GeoDataFrames.
+    Note: is not resilient to empty graphs, which can be returned by OSMnx for example.
+    """
+    nodes, edges = ox.graph_to_gdfs(g)
+    nodes = nodes.rename(columns={"x": "lon", "y": "lat"}).drop(columns=["modes"])
+    nodes.index.name = "node_id"
+
+    edges = edges.rename(columns={"u": "source", "v": "target"}).drop(columns=["mode"]).reset_index(level=-1, drop=True)
+    return nodes, edges
+
+
+def graph_to_data(g):
+    """Converts a graph to nodes and edges DataFrames"""
+    edges_df = nx.to_pandas_edgelist(g).set_index(['source', 'target'])
+    node_df = pd.DataFrame.from_dict(dict(g.nodes(data=True)), orient='index')['boarding_cost'].to_frame()
+    node_df.index.name = 'node_id'
+
+    return node_df, edges_df
 
 
 def pad_array(array, pad_val=None, step=1, n_pads=1):
@@ -48,21 +70,14 @@ def pad_array(array, pad_val=None, step=1, n_pads=1):
 ################### Plotting Functions #################################################
 ########################################################################################
 
-def graph_to_dfs(g):
-    nodes, edges = ox.graph_to_gdfs(g)
-    nodes = nodes.rename(columns={"x": "lon", "y": "lat"})
-    nodes.index.name = "name"
-
-    edges = edges.rename(columns={"u": "from", "v": "to"})
-    return nodes, edges
-
 
 def linestring_to_coords(linestring, dim):
     lat, long = linestring.xy
     return [np.linspace(lat[0], lat[1], dim + 2)[1:-1], np.linspace(long[0], long[1], dim + 2)[1:-1]]
 
 
-def plot_graph_map(g, marker_style=None, num_edge_markers=0, **fig_style_kwargs):
+def plot_graph_map(g, marker_style=None, num_edge_markers=0,
+                   **fig_style_kwargs):  # todo: remove nodes and edges import and add them as parameters
     if marker_style is None:
         marker_style = dict(size=10, color="red")
     if "margin" not in fig_style_kwargs.keys():
@@ -70,7 +85,7 @@ def plot_graph_map(g, marker_style=None, num_edge_markers=0, **fig_style_kwargs)
     if "mapbox" not in fig_style_kwargs.keys():
         fig_style_kwargs["mapbox"] = {'style': "open-street-map", 'zoom': 2}
 
-    nodes, edges = graph_to_dfs(g)
+    nodes, edges = graph_to_gdfs(g)
     nodes_dict = nodes[["lon", "lat"]].reset_index().to_dict(orient="list")
     nodes_names, nodes_lats, nodes_longs = nodes_dict["name"], nodes_dict["lat"], nodes_dict["lon"]
 
@@ -127,11 +142,6 @@ def plot_graph_map(g, marker_style=None, num_edge_markers=0, **fig_style_kwargs)
 ################### Graph Evaluation ###################################################
 ########################################################################################
 
-def load_node_df(graph):
-    node_df = pd.DataFrame.from_dict(dict(graph.nodes(data=True)), orient='index')['boarding_cost'].to_frame()
-    node_df.index.name = 'node_id'
-    return node_df
-
 
 def nodes_centrality_evaluation(graph, node_df):
     """
@@ -178,7 +188,7 @@ def evaluate_graph(graph, node_df=None):
 
     # Extraction of nodes
     if node_df is None:
-        node_df = load_node_df(graph)
+        node_df, _ = graph_to_data(graph)
 
     # Evaluation of centrality
     node_df = nodes_centrality_evaluation(graph, node_df)
@@ -208,11 +218,11 @@ def evaluate_graph(graph, node_df=None):
 ################### Graph Attack #######################################################
 ########################################################################################
 
-def attack_graph(graph_in, target=None, n_steps=10):
+def attack_graph(graph_in, target=None, n_steps=10, min_threshold=0.05):
     graph = graph_in.copy()
     graph_stats, node_df = evaluate_graph(graph)
     total_nodes = graph_stats['nodes']
-    amount = int(total_nodes / n_steps)
+    amount = int((total_nodes - total_nodes * min_threshold) / n_steps)
 
     results = {'100%': graph_stats.values}
     for i in range(n_steps - 1):
@@ -240,9 +250,10 @@ def plot_attack_result(results, title, cols_to_plot=None):
     elif (len(cols_to_plot) % 2) != 0:
         raise ValueError('cols_to_plot must have an even number of elements')
 
-    full_fig = make_subplots(rows=int(len(cols_to_plot)/2), cols=2, subplot_titles=cols_to_plot, shared_xaxes=True)
+    full_fig = make_subplots(rows=int(len(cols_to_plot) / 2), cols=2, subplot_titles=cols_to_plot, shared_xaxes=True)
     for i, col in enumerate(cols_to_plot):
-        full_fig.add_trace(go.Scatter(x=results.index, y=results[col], mode='markers+lines', name=col, hoverinfo="y"), row=i//2 + 1, col=i%2 + 1)
+        full_fig.add_trace(go.Scatter(x=results.index, y=results[col], mode='markers+lines', name=col, hoverinfo="y"),
+                           row=i // 2 + 1, col=i % 2 + 1)
 
     full_fig.update_layout(title=title, showlegend=False, title_xanchor="center",
                            title_yanchor="top", title_y=0.9, title_x=0.5)
@@ -252,3 +263,30 @@ def plot_attack_result(results, title, cols_to_plot=None):
                          ).update_layout(title_xanchor="center", title_yanchor="top", title_y=0.9, title_x=0.5)
 
     return full_fig, scaled_fig
+
+
+def attacks_results_summary(results_list, summary_col='weak_GC', threshold=0.05, names_list=None, save=False, n_steps=10):
+    if names_list is None:
+        names_list = ['random', 'centrality', 'degree', 'betweenness', 'closeness', 'eigenvector', 'pagerank']
+
+    dead_points = pd.Series(name='dead_point')
+    summary_df = pd.DataFrame(index=results_list[0].index)
+    iterator = zip(names_list, results_list)
+
+    for name, results in iterator:
+        dead_point = np.sum(results[summary_col] >= threshold)
+        dead_points[name] = dead_point
+        summary_df[name] = results[summary_col]
+
+    if save:
+        summary_df.to_csv(os.path.join(ATTACKS_PATH, f'summary_df_{n_steps}.csv'))
+        dead_points.to_csv(os.path.join(ATTACKS_PATH, f'summary_dps_{n_steps}.csv'))
+
+    summary_plot = px.line(summary_df, title=f'{summary_col.upper()} Component for different attacks', labels={'value': 'Weak GC', 'index': 'Timestep'})
+    bar_plot = px.bar(dead_points, title=f'Dead Graph Timestep for different attacks based on {summary_col.upper()}',
+                      labels={'value': 'Dead Timestep', 'index': 'Attack type'})
+
+
+    return dead_points, summary_df, bar_plot, summary_plot
+
+
