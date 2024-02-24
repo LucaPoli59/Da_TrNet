ac6465c08d31d120cf991d74d8eb62fad2bdab0f
Luca Poli <poli.luca.marconi@gmail.com>
2024-02-24 15:23:12 +0100

    prepare for deployment

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
 import diskcache
 from dash import DiskcacheManager, html
+from whitenoise import WhiteNoise
 
 cache = diskcache.Cache("/.cache")
 background_callback_manager = DiskcacheManager(cache)
@@ -11,6 +12,9 @@ app = dash.Dash(__name__, use_pages=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP],
                 meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}],
                 suppress_callback_exceptions=True, background_callback_manager=background_callback_manager)
+server = app.server
+server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/')
+
 
 app.layout = html.Div([
     html.Div([
